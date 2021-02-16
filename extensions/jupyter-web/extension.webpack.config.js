454e827fb548fd9b0a882fc05a5769ba5ea82b5c
Siddhant Khare <siddhantkhare2694@gmail.com>
2021-02-16 12:16:44 +0530

    fixed extension.webpack.config.js

--- extensions/jupyter-web/extension.webpack.config.js
+++ extensions/jupyter-web/extension.webpack.config.js
@@ -9,67 +9,69 @@
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-import { join } from "path";
+const path = require("path");
 
-export const context = __dirname;
-export const mode = "none";
-export const target = "webworker";
-export const entry = {
-	extension: "./src/extension.js",
-};
-export const resolve = {
-	// node: { fs: 'empty', child_process: 'empty', net: 'empty', tls: 'empty' },
-	fallback: {
-		fs: false,
-		child_process: false,
-		net: false,
-		tls: false,
-		stream: false,
-		assert: false,
-		url: false,
-		buffer: false,
-		querystring: false,
-		zlib: false,
-		os: false,
-		crypto: require.resolve("crypto-browserify"),
+module.exports = /** @type WebpackConfig */ {
+	context: __dirname,
+	mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+	target: "webworker", // extensions run in a webworker context
+	entry: {
+		extension: "./src/extension.js",
 	},
-	mainFields: ["module", "main"],
-	extensions: [".ts", ".js"],
-	alias: {
-		https: "https-browserify",
-		http: "http-browserify",
+	resolve: {
+		// node: { fs: 'empty', child_process: 'empty', net: 'empty', tls: 'empty' },
+		fallback: {
+			fs: false,
+			child_process: false,
+			net: false,
+			tls: false,
+			stream: false,
+			assert: false,
+			url: false,
+			buffer: false,
+			querystring: false,
+			zlib: false,
+			os: false,
+			crypto: require.resolve("crypto-browserify"),
+		},
+		mainFields: ["module", "main"],
+		extensions: [".ts", ".js"], // support ts-files and js-files
+		alias: {
+			https: "https-browserify",
+			http: "http-browserify",
+		},
 	},
-};
-export const module = {
-	rules: [
-		{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [
-				{
-					// configure TypeScript loader:
-					// * enable sources maps for end-to-end source maps
-					loader: "ts-loader",
-					options: {
-						compilerOptions: {
-							sourceMap: true,
-							declaration: false,
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						// configure TypeScript loader:
+						// * enable sources maps for end-to-end source maps
+						loader: "ts-loader",
+						options: {
+							compilerOptions: {
+								sourceMap: true,
+								declaration: false,
+							},
 						},
 					},
-				},
-			],
-		},
-	],
+				],
+			},
+		],
+	},
+	externals: {
+		vscode: "commonjs vscode", // ignored because it doesn't exist
+	},
+	performance: {
+		hints: false,
+	},
+	output: {
+		filename: "extension.js",
+		path: path.join(__dirname, "dist"),
+		libraryTarget: "commonjs",
+	},
+	devtool: "source-map",
 };
-export const externals = {
-	vscode: "commonjs vscode", // ignored because it doesn't exist
-};
-export const performance = {
-	hints: false,
-};
-export const output = {
-	filename: "extension.js",
-	path: join(__dirname, "dist"),
-	libraryTarget: "commonjs",
-};
-export const devtool = "source-map";
