e5c7b9e79a6b643332fc819092290c1465f9dbc7
netcon <netcon@live.com>
2021-02-21 00:38:04 +0800

    feat: github submodules support #143 (#150)
    * feat: github submodules support #143
    
    * feat: support ssh format url in .gitmodules

--- extensions/github1s/src/util/index.ts
+++ extensions/github1s/src/util/index.ts
@@ -19,6 +19,7 @@ export {
 	getRepositoryTags,
 	changeCurrentRef,
 } from './git-ref';
+export { parseGitmodules, parseSubmoduleUrl } from './submodule';
 
 export const noop = () => {};
 
---
+++ extensions/github1s/src/util/submodule.ts
@@ -0,0 +1,104 @@
+/**
+ * @file git submodule util
+ * @author netcon
+ */
+
+import { FileSystemError, Uri } from 'vscode';
+
+// the code below is come from https://github.com/microsoft/vscode/blob/1.52.1/extensions/git/src/git.ts#L661
+export interface Submodule {
+	name: string;
+	path: string;
+	url: string;
+}
+
+// the code below is come from https://github.com/microsoft/vscode/blob/1.52.1/extensions/git/src/git.ts#L667
+export function parseGitmodules(raw: string): Submodule[] {
+	const regex = /\r?\n/g;
+	let position = 0;
+	let match: RegExpExecArray | null = null;
+
+	const result: Submodule[] = [];
+	let submodule: Partial<Submodule> = {};
+
+	function parseLine(line: string): void {
+		const sectionMatch = /^\s*\[submodule "([^"]+)"\]\s*$/.exec(line);
+
+		if (sectionMatch) {
+			if (submodule.name && submodule.path && submodule.url) {
+				result.push(submodule as Submodule);
+			}
+
+			const name = sectionMatch[1];
+
+			if (name) {
+				submodule = { name };
+				return;
+			}
+		}
+
+		if (!submodule) {
+			return;
+		}
+
+		const propertyMatch = /^\s*(\w+)\s*=\s*(.*)$/.exec(line);
+
+		if (!propertyMatch) {
+			return;
+		}
+
+		const [, key, value] = propertyMatch;
+
+		switch (key) {
+			case 'path':
+				submodule.path = value;
+				break;
+			case 'url':
+				submodule.url = value;
+				break;
+		}
+	}
+
+	while ((match = regex.exec(raw))) {
+		parseLine(raw.substring(position, match.index));
+		position = match.index + match[0].length;
+	}
+
+	parseLine(raw.substring(position));
+
+	if (submodule.name && submodule.path && submodule.url) {
+		result.push(submodule as Submodule);
+	}
+
+	return result;
+}
+
+export const parseSubmoduleUrl = (url: string) => {
+	try {
+		let authority = '';
+		let path = '';
+		// the url is looks like git@github.com:xcv58/rc_config_files.git
+		if (url.startsWith('git@')) {
+			const colonIndex = url.indexOf(':');
+			authority = url.slice(0, colonIndex);
+			path = url.slice(colonIndex + 1);
+		} else {
+			const submoduleUri = Uri.parse(url);
+			authority = submoduleUri.authority;
+			path = submoduleUri.path;
+		}
+		if (!/\bgithub\.com$/.test(authority)) {
+			throw FileSystemError.Unavailable(
+				'only github submodules are supported now'
+			);
+		}
+		const [submoduleOwner, submoduleRepoPart] = path.split('/').filter(Boolean);
+		// if there are a repo which the name endsWith '.git' (likes conwnet/demo.git), this ambiguity may cause a problem
+		const submoduleRepo = submoduleRepoPart.endsWith('.git')
+			? submoduleRepoPart.slice(0, -4)
+			: submoduleRepoPart;
+		return [submoduleOwner, submoduleRepo];
+	} catch (e) {
+		throw FileSystemError.Unavailable('Can not found valid submodule declare');
+	}
+};
