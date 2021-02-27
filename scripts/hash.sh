cee6ed908c777917e3e9fa4fc3c98e2d3b1af79f
netcon <netcon@live.com>
2021-02-27 17:00:47 +0800

    feat: use vercel deploy production

--- scripts/hash.sh
+++ scripts/hash.sh
@@ -6,12 +6,13 @@ APP_ROOT=$(pwd)
 
 COMMIT_ID=$(git rev-parse HEAD)
 SHORT_COMMIT_ID=${COMMIT_ID:0:7}
-STATIC_FOLDER_NAME="static-${SHORT_COMMIT_ID-unknown}"
+STATIC_HASH_CODE="${SHORT_COMMIT_ID-unknown}"
 
 if [ -e "${APP_ROOT}/dist/static" ]; then
-	mv "${APP_ROOT}/dist/static" "${APP_ROOT}/dist/${STATIC_FOLDER_NAME}"
-	sed "s/{STATIC_FOLDER}/${STATIC_FOLDER_NAME}/" "${APP_ROOT}/resources/index-hash.html" > "${APP_ROOT}/dist/404.html"
-	rm -f "${APP_ROOT}/dist/index.html" # remove the exists `index.html` file
+	mv "${APP_ROOT}/dist/static" "${APP_ROOT}/dist/${STATIC_HASH_CODE}"
+	mkdir -p "${APP_ROOT}/dist/static"
+	mv "${APP_ROOT}/dist/${STATIC_HASH_CODE}" "${APP_ROOT}/dist/static/${STATIC_HASH_CODE}"
+	sed "s/{STATIC_HASH_CODE}/${STATIC_HASH_CODE}/" "${APP_ROOT}/resources/index-hash.html" > "${APP_ROOT}/dist/index.html"
 	echo "hash static files done"
 else
 	echo "Please build github1s first"
