58002f36da727cb8901b9b4e1ba015cd4a7283a4
netcon <netcon@live.com>
2021-03-05 18:07:58 +0800

    feat: enable code search and add sponsor Sourcegraph

--- extensions/github1s/src/sponsors.ts
+++ extensions/github1s/src/sponsors.ts
@@ -11,6 +11,11 @@ const sponsors = [
 		link: 'https://vercel.com/?utm_source=vscode-github1s&utm_campaign=oss',
 		description: 'Develop. Preview. Ship.',
 	},
+	{
+		name: 'Sourcegraph',
+		link: 'https://sourcegraph.com',
+		description: 'Universal code search',
+	},
 ];
 
 export const showSponsors = () => {
