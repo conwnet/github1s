8f34a0ea946e530824e396157740e0788890d3cb
Josh Soref <jsoref@users.noreply.github.com>
2021-03-21 22:41:33 -0400

    spelling: persist
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- extensions/github1s/src/vscode.proposed.d.ts
+++ extensions/github1s/src/vscode.proposed.d.ts
@@ -1329,13 +1329,13 @@ declare module 'vscode' {
 	export interface NotebookDocumentContentOptions {
 		/**
 		 * Controls if outputs change will trigger notebook document content change and if it will be used in the diff editor
-		 * Default to false. If the content provider doesn't persisit the outputs in the file document, this should be set to true.
+		 * Default to false. If the content provider doesn't persist the outputs in the file document, this should be set to true.
 		 */
 		transientOutputs: boolean;
 
 		/**
 		 * Controls if a metadata property change will trigger notebook document content change and if it will be used in the diff editor
-		 * Default to false. If the content provider doesn't persisit a metadata property in the file document, it should be set to true.
+		 * Default to false. If the content provider doesn't persist a metadata property in the file document, it should be set to true.
 		 */
 		transientMetadata: { [K in keyof NotebookCellMetadata]?: boolean };
 	}
