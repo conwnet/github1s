ebf7b260c7bc9dec3f2f5dc4e71d1c011b665ee6
netcon <netcon@live.com>
2021-05-17 20:25:06 +0800

    feat: open settings view if current token is invalid

--- extensions/github1s/src/helpers/fetch.ts
+++ extensions/github1s/src/helpers/fetch.ts
@@ -108,6 +108,10 @@ export const fetch = reuseable(async (url: string, options?: RequestInit) => {
 	}
 	if (response.status === 401) {
 		return response.json().then((data) => {
+			// current token is invalid
+			if (data.message === 'Bad credentials') {
+				vscode.commands.executeCommand('github1s.views.settings.focus');
+			}
 			throw new RequestInvalidTokenError(data.message, token);
 		});
 	}
