4bcd895f7c5ef9bb70924875748fcb5fb33ac59f
Bo Zhang <bo@bytelegend.com>
2021-12-23 07:04:11 +0800

    Fix up-to-date check issue in copy-extensions.sh
    Previously the script only check if `extensions/emmet/dist/browser`
    exists, and skip compiling if so. This PR adds an extra `git diff`
    check, and run compiling if there's any changes in `extensions`.

--- vscode-web-github1s/scripts/copy-extensions.sh
+++ vscode-web-github1s/scripts/copy-extensions.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 
 cd "$(dirname "${0}")/.."
 APP_ROOT=$(pwd)
-
 function ensureBuiltinExtensitions() {
 	cd "${APP_ROOT}/lib/vscode"
-	if [ ! -e "extensions/emmet/dist/browser" ]
+	git diff --exit-code --name-only extensions
+	if [ $? != 0 ] || [ ! -e "extensions/emmet/dist/browser" ]
 	then
 		echo "compile vscode builtin extensions..."
 		yarn gulp compile-web
 		yarn gulp compile-extension-media
+	else
+		echo "vscode builtin extensions is up-to-date, skip compiling."
 	fi
 }
 
@@ -24,3 +26,4 @@ function main() {
 }
 
 main "$@"
+
