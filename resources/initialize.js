5326b902faf786410721da750c6a33deb3397edb
netcon <netcon@live.com>
2022-04-16 17:57:52 +0800

    feat: add auth command

--- resources/initialize.js
+++ resources/initialize.js
@@ -1,4 +1,37 @@
 (function () {
+	// resolves with `{ access_token: string; token_type?: string; scope?: string } | { error: string; error_description: string; }`
+	const getGitHubAccessToken = () => {
+		const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?scope=repo,user:email&client_id=eae6621348403ea49103`;
+		const OPEN_WINDOW_FEATURES =
+			'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=520,top=150,left=150';
+		const AUTH_PAGE_ORIGIN = 'https://auth.github1s.com';
+		const opener = window.open(GITHUB_AUTH_URL, '_blank', OPEN_WINDOW_FEATURES);
+		const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+		return new Promise((resolve) => {
+			const handleAuthMessage = (event) => {
+				// Note that though the browser block opening window and popup a tip,
+				// the user can be still open it from the tip. In this case, the `opener`
+				// is null, and we should still process the authorizing message
+				const isValidOpener = !!(opener && event.source === opener);
+				const isValidOrigin = event.origin === AUTH_PAGE_ORIGIN;
+				const isValidResponse = event.data ? event.data.type === 'authorizing' : false;
+				if (!isValidOpener || !isValidOrigin || !isValidResponse) {
+					return;
+				}
+				window.removeEventListener('message', handleAuthMessage);
+				resolve(event.data.payload);
+			};
+
+			window.addEventListener('message', handleAuthMessage);
+			// if there isn't any message from opener window in 300s, remove the message handler
+			timeout(300 * 1000).then(() => {
+				window.removeEventListener('message', handleAuthMessage);
+				resolve({ error: 'authorizing_timeout', error_description: 'Authorizing timeout' });
+			});
+		});
+	};
+
 	const staticAssetsPath = '/static' + (window.staticHashCode ? '/' + window.staticHashCode : '');
 	const staticAssetsPrefix = window.location.origin + staticAssetsPath;
 	const nodeModulesPrefix = staticAssetsPrefix + '/node_modules';
@@ -115,6 +148,10 @@
 				handler(url) {
 					window.history.pushState(null, '', url);
 				},
+			},
+			{
+				id: 'github1s.commands.vscode.getGitHubAccessToken',
+				handler: getGitHubAccessToken,
 			},
 		],
 		product: productJson,
