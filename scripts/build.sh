2d9e53951fe9ce5674013f3ad40d04609fe76574
xcv58 <i@xcv58.com>
2022-07-05 23:31:20 -0400

    Revert to master

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "${0}")/.."
+APP_ROOT=$(pwd)
+
+# execute all necessary tasks
+function main() {
+	rm -rf "${APP_ROOT}/dist"
+	cd "${APP_ROOT}"
+	./scripts/build/build-github1s-extensions.sh
+	yarn webpack
+
+	echo "all build done!"
+}
+
+main "$@"
