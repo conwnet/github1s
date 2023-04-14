f00d3a764eb7d7bbca6d13ec1d0b1af07ef44b6e
netcon <netcon@live.com>
2023-04-14 16:16:40 +0800

    chore: bump vscode to 1.77.3 (#493)

--- vscode-web/src/vs/workbench/contrib/files/browser/editors/fileEditorInput.ts
+++ vscode-web/src/vs/workbench/contrib/files/browser/editors/fileEditorInput.ts
@@ -181,9 +181,9 @@ export class FileEditorInput extends AbstractTextResourceEditorInput implements 
 	}
 
 	private allowLabelOverride(): boolean {
-    /* below codes are changed by github1s */
-    if (window?.vscodeWeb?.allowEditorLabelOverride) return true;
-    /* above codes are changed by github1s */
+		/* below codes are changed by github1s */
+		if (window?.vscodeWeb?.allowEditorLabelOverride) return true;
+		/* above codes are changed by github1s */
 		return this.resource.scheme !== this.pathService.defaultUriScheme &&
 			this.resource.scheme !== Schemas.vscodeUserData &&
 			this.resource.scheme !== Schemas.file &&
