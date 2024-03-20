d5596ebac678dd85c16ded21c15e7bc026e4ab62
vitalmotif <zhangshengqiang@outlook.com>
2024-03-20 15:19:34 +0800

    chore: fix some typos
    Signed-off-by: vitalmotif <zhangshengqiang@outlook.com>

--- src/notification.ts
+++ src/notification.ts
@@ -9,7 +9,7 @@ const NOTIFICATION_STORAGE_KEY = 'GITHUB1S_NOTIFICATION';
 // Change this if a new notification should be shown
 const NOTIFICATION_STORAGE_VALUE = '20210212';
 
-/*** begin notificaton block ***/
+/*** begin notification block ***/
 export const renderNotification = (platform: string) => {
 	// If user has confirmed the notification and checked `don't show me again`, ignore it
 	if (window.localStorage.getItem(NOTIFICATION_STORAGE_KEY) === NOTIFICATION_STORAGE_VALUE) {
