98a8ee99259083ef726f69c904f1090054dd3ba9
netcon <netcon@live.com>
2026-08-05 16:29:50 +0800

    feat: update github oauth (#707)

--- src/github-auth.ts
+++ src/github-auth.ts
@@ -3,17 +3,33 @@
  * @author netcon
  */
 
-const CLIENT_ID = 'eae6621348403ea49103';
 const GITHUB_ORIGIN = 'https://github.com';
-const GITHUB_AUTH_URL = `${GITHUB_ORIGIN}/login/oauth/authorize?scope=repo,user:email&client_id=${CLIENT_ID}`;
+const AUTH_PAGE_ORIGIN = 'https://auth.github1s.com';
+const AUTH_REDIRECT_URI = `${AUTH_PAGE_ORIGIN}/api/github-auth-callback`;
+const CLIENT_ID = 'eae6621348403ea49103';
 const OPEN_WINDOW_FEATURES =
 	'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=520,top=150,left=150';
-const AUTH_PAGE_ORIGIN = 'https://auth.github1s.com';
+
+export const createRandomString = (length: number) => {
+	const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	return Array.from({ length }, () => charset.charAt(Math.floor(Math.random() * charset.length))).join('');
+};
+
+const createAuthorizeUrl = (state: string) => {
+	const parameters = Object.entries({
+		state,
+		scope: 'repo,user:email',
+		client_id: CLIENT_ID,
+		redirect_uri: AUTH_REDIRECT_URI,
+	}).map(([key, value]) => `${key}=${encodeURIComponent(value)}`);
+	return `${GITHUB_ORIGIN}/login/oauth/authorize?${parameters.join('&')}`;
+};
 
 export const timeout = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const ConnectToGitHub = () => {
-	const opener = window.open(GITHUB_AUTH_URL, '_blank', OPEN_WINDOW_FEATURES);
+	const STATE = createRandomString(32);
+	const opener = window.open(createAuthorizeUrl(STATE), '_blank', OPEN_WINDOW_FEATURES);
 
 	return new Promise((resolve) => {
 		const handleAuthMessage = (event: MessageEvent) => {
